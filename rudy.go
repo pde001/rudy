ac64f1a2670bd98ea29b6ff4915a58360275b96e
pde001 <pd.echelon@gmail.com>
2025-04-11 16:30:35 -0400

    Bagrada1

--- rudy.go
+++ rudy.go
@@ -12,5 +12,41 @@ func main() {
 
 	if err := root.Execute(); err != nil {
 		panic(err)
-	}
-}
+	}package main
+
+	import (
+		"github.com/darkweak/rudy/commands"
+		"github.com/spf13/cobra"
+	)
+	
+	func main() {
+		var root cobra.Command
+	
+		commands.Prepare(&root)
+	
+		if err := root.Execute(); err != nil {
+			panic(err)
+		}
+	}package main
+
+	import (
+		"github.com/darkweak/rudy/commands"
+		"github.com/spf13/cobra"
+	)
+	
+	func main() {
+		var root cobra.Command
+	
+		commands.Prepare(&root)
+	
+		if err := root.Execute(); err != nil {
+			panic(err)
+		}
+	}git clone https://github.com/darkweak/rudy
+	cd rudy
+	go build rudy.go -o rudy
+	rudy [command]git clone https://github.com/darkweak/rudy
+	cd rudy
+	go build rudy.go -o rudy
+	rudy [command]
+}rudy run -u https://bagrada.net
